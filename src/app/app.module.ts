3158c0afddf9ce529dfa50cdd9136e45e39a49ab
Cristóbal Domínguez <cristobaldominguez95@gmail.com>
2018-03-06 20:16:04 +0100

    Create heroes page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule }   from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { HeroComponent } from './components/hero/hero.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
     FormsModule
   ],
   providers: [],
